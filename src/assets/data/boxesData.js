135dd1ccdae1556f3824e496ad6583611b2aab49
sakshidonadkar <you@example.com>
2025-01-03 17:56:23 +0530

    update

--- src/assets/data/boxesData.js
+++ src/assets/data/boxesData.js
@@ -1,83 +1,108 @@
 // boxesData.js
+import RoundClock from './../images/RoundClock.png';
+import DigitalClock from './../images/DigitalClock.png';
 export const boxesData = [
-    {
-      title: "ROUND CLOCK",
-      imageSrc: "https://via.placeholder.com/200",
-      liveLink: "https://example.com/live1",
-      githubLink: "https://github.com/example1",
-    },
-    {
-      title: "Digital Clock",
-      imageSrc: "https://via.placeholder.com/200",
-      liveLink: "https://example.com/live2",
-      githubLink: "https://github.com/example2",
-    },
-    {
-        title: "Box 1",
-        imageSrc: "https://via.placeholder.com/200",
-        liveLink: "https://example.com/live1",
-        githubLink: "https://github.com/example1",
-      },
-      {
-        title: "Box 2",
-        imageSrc: "https://via.placeholder.com/200",
-        liveLink: "https://example.com/live2",
-        githubLink: "https://github.com/example2",
-      },
-      {
-        title: "Box 1",
-        imageSrc: "https://via.placeholder.com/200",
-        liveLink: "https://example.com/live1",
-        githubLink: "https://github.com/example1",
-      },
-      {
-        title: "Box 2",
-        imageSrc: "https://via.placeholder.com/200",
-        liveLink: "https://example.com/live2",
-        githubLink: "https://github.com/example2",
-      },
-      {
-        title: "Box 1",
-        imageSrc: "https://via.placeholder.com/200",
-        liveLink: "https://example.com/live1",
-        githubLink: "https://github.com/example1",
-      },
-      {
-        title: "Box 2",
-        imageSrc: "https://via.placeholder.com/200",
-        liveLink: "https://example.com/live2",
-        githubLink: "https://github.com/example2",
-    },
-    {
-        title: "Box 2",
-        imageSrc: "https://via.placeholder.com/200",
-        liveLink: "https://example.com/live2",
-        githubLink: "https://github.com/example2",
-      },
-      {
-        title: "Box 1",
-        imageSrc: "https://via.placeholder.com/200",
-        liveLink: "https://example.com/live1",
-        githubLink: "https://github.com/example1",
-      },
-      {
-        title: "Box 2",
-        imageSrc: "https://via.placeholder.com/200",
-        liveLink: "https://example.com/live2",
-        githubLink: "https://github.com/example2",
-      },
-      {
-        title: "Box 1",
-        imageSrc: "https://via.placeholder.com/200",
-        liveLink: "https://example.com/live1",
-        githubLink: "https://github.com/example1",
-      },
-      {
-        title: "Box 2",
-        imageSrc: "https://via.placeholder.com/200",
-        liveLink: "https://example.com/live2",
-        githubLink: "https://github.com/example2",
-      },
-    // Add the other boxes similarly...
-  ];
-  
+  {
+    title: "Round Clock",
+    imageSrc: RoundClock,
+    liveLink: "https://round-clock-jade.vercel.app",
+    githubLink: "https://github.com/sakshidonadkar/Round_Clock",
+  },
+  {
+    title: "Digital Clock",
+    imageSrc: DigitalClock,
+    liveLink: "https://example.com/live2",
+    githubLink: "https://github.com/example2",
+  },
+  {
+    title: "Stopwatch",
+    imageSrc: "https://via.placeholder.com/200",
+    liveLink: "https://example.com/live1",
+    githubLink: "https://github.com/example1",
+  },
+  {
+    title: "Weather App",
+    imageSrc: "https://via.placeholder.com/200",
+    liveLink: "https://example.com/live2",
+    githubLink: "https://github.com/example2",
+  },
+  {
+    title: "Currency Converter",
+    imageSrc: "https://via.placeholder.com/200",
+    liveLink: "https://example.com/live1",
+    githubLink: "https://github.com/example1",
+  },
+  {
+    title: "Password Generator",
+    imageSrc: "https://via.placeholder.com/200",
+    liveLink: "https://example.com/live2",
+    githubLink: "https://github.com/example2",
+  },
+  {
+    title: "Guess a Number",
+    imageSrc: "https://via.placeholder.com/200",
+    liveLink: "https://example.com/live1",
+    githubLink: "https://github.com/example1",
+  },
+  {
+    title: "Guess a Word",
+    imageSrc: "https://via.placeholder.com/200",
+    liveLink: "https://example.com/live2",
+    githubLink: "https://github.com/example2",
+  },
+  {
+    title: "Guess Future GF",
+    imageSrc: "https://via.placeholder.com/200",
+    liveLink: "https://example.com/live2",
+    githubLink: "https://github.com/example2",
+  },
+  {
+    title: "Tic Tac Toe",
+    imageSrc: "https://via.placeholder.com/200",
+    liveLink: "https://example.com/live1",
+    githubLink: "https://github.com/example1",
+  },
+  {
+    title: "Rock Paper Scissor",
+    imageSrc: "https://via.placeholder.com/200",
+    liveLink: "https://example.com/live2",
+    githubLink: "https://github.com/example2",
+  },
+  {
+    title: "Todo App",
+    imageSrc: "https://via.placeholder.com/200",
+    liveLink: "https://example.com/live1",
+    githubLink: "https://github.com/example1",
+  },
+  {
+    title: "Drum Kit",
+    imageSrc: "https://via.placeholder.com/200",
+    liveLink: "https://example.com/live2",
+    githubLink: "https://github.com/example2",
+  },
+  {
+    title: "Love Calculator",
+    imageSrc: "https://via.placeholder.com/200",
+    liveLink: "https://example.com/live2",
+    githubLink: "https://github.com/example2",
+  },
+  {
+    title: "Find a Recipe",
+    imageSrc: "https://via.placeholder.com/200",
+    liveLink: "https://example.com/live2",
+    githubLink: "https://github.com/example2",
+  },
+  {
+    title: "Test Your Memory",
+    imageSrc: "https://via.placeholder.com/200",
+    liveLink: "https://example.com/live2",
+    githubLink: "https://github.com/example2",
+  },
+  {
+    title: " Play Piano",
+    imageSrc: "https://via.placeholder.com/200",
+    liveLink: "https://example.com/live2",
+    githubLink: "https://github.com/example2",
+  },
+  // Add the other boxes similarly...
+];
--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,11 +1,10 @@
-// Container.js
 import React from "react";
 import { boxesData } from "./../assets/data/boxesData.js";
 import Box from "./Box";
 
 const Container = () => {
   return (
-    <div className="container mx-auto p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+    <div className="container mx-auto p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-12">
       {boxesData.map((box, index) => (
         <Box
           key={index}
